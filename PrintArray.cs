30f27b222a5754665cba2c9ea797cab04247aeff
suraj807 <surajdev805@gmail.com>
2022-04-03 00:05:30 +0530

    print array int/double/char

---
+++ PrintArray.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo
+{
+    internal class PrintArray
+    {
+      public static void printAnArray(int[] arr)
+        {
+            Console.WriteLine("\ninteger array element");
+            foreach (int element in arr)
+            {
+                Console.WriteLine(element);
+            }
+        }
+        public static void printAnArray(double[] arr)
+        {
+            Console.WriteLine("\ndouble array element");
+            foreach (double element in arr)
+            {
+                Console.WriteLine(element);
+            }
+        }
+        public static void printAnArray(char[]arr)
+        {
+            Console.WriteLine("\nchar array element");
+            foreach (char element in arr)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intarr = { 1, 2, 3 };
+            double[] doublearr = {12.2,125.2,45.4,5.2,23.5};
+            char[] chararr = { 'a', 'b', 'c', 'd', 'e', 'f', };
+            PrintArray.printAnArray(intarr);
+            PrintArray.printAnArray(doublearr);
+            PrintArray.printAnArray(chararr);
+            Console.ReadLine();
+        }
+    }
+}
